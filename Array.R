310546b2a26a4725519ce58b572ae3557c6d1fdd
Vishal Dixit <53947968+Vishal-Dixit-0611@users.noreply.github.com>
2021-02-17 19:36:00 +0530

    factor commit

--- Array.R
+++ Array.R
@@ -3,3 +3,12 @@ c1 = c("Item","Serial","Size")
 d1 = c("DataCenter EU","Datacenter US")
 a <- array(1:9,dim=c(3,3,2),dimnames=list(r1,c1,d1))
 a
+
+# factor
+
+f1 <- factor(c("red","blue","black","purple"))
+f1
+as.numeric(f1)
+f1<-relevel (f1,ref = "red")
+f1
+as.numeric(f1)
